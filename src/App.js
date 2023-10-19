1b25c3e04266706f3dd9b0a7c3a3df394fb1c4f1
Hetu Patel <hetu200211@gmail.com>
2023-10-19 14:44:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [task, setTask] = useState(["1", "2", "3", "4", "5"]);
+  const [completed, setCompleted] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <div className="top">
+          <h1>TODO</h1>
+        </div>
+        <div className="bottom">
+          <div className="heading">
+            <p>Task list</p>
+            <p>
+              {completed}/{task.length} done
+            </p>
+          </div>
+          <div className="list">
+            {task.map((e, index) => {
+              return (
+                <div className="item" key={index}>
+                  <span>-</span>
+                  <input
+                    type="checkbox"
+                    onClick={(e) => {
+                      if (e.target.checked) {
+                        setCompleted(completed + 1);
+                        const ele = document.getElementById(`item-${index}`);
+                        ele.style.textDecoration = "line-through";
+                      } else {
+                        setCompleted(completed - 1);
+                        const ele = document.getElementById(`item-${index}`);
+                        ele.style.textDecoration = "none";
+                      }
+                    }}
+                  ></input>{" "}
+                  <p id={`item-${index}`}>{e}</p>
+                </div>
+              );
+            })}
+          </div>
+          <button>
+            <span>+</span>Add task
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
